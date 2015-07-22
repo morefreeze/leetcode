c8eb4c06cc50125f6b189d7de3a1999f8144ac9c
MoreFreeze <liuchenxing@acfun.tv>
2015-07-22 15:00:57 +0800

    add count and say, a secret is no 4 in sequence

---
+++ algo/count_say.cpp
@@ -0,0 +1,101 @@
+#ifndef COUNT_SAY_H
+
+#define COUNT_SAY_H
+
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+//conversion
+//------------------------------------------
+inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
+template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return sout.str();}
+
+//math
+//-------------------------------------------
+template<class T> inline T sqr(T x) {return x*x;}
+
+//typedef
+//------------------------------------------
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+//container util
+//------------------------------------------
+#define ALL(a)  (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define EACH(i,c) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); ++i)
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+//------------------------------------------
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//constant
+//--------------------------------------------
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+
+//clear memory
+#define CLR(a) memset((a), 0 ,sizeof(a))
+
+//debug
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+
+class Solution {
+    public:
+        string countAndSay(int n) {
+            string ans[2];
+            ans[0] = "1";
+            REP (i, n-1){
+                string &cur = ans[i%2];
+                ostringstream osout(ans[1-i%2]);
+                int cur_cnt(1);
+                int cur_num(cur[0]-'0');
+                FOR (j, 1, SZ(cur)){
+                    if (cur[j] == cur[j-1]){
+                        ++cur_cnt;
+                    }
+                    else{
+                        osout << cur_cnt << cur_num;
+                        cur_cnt = 1;
+                        cur_num = cur[j] - '0';
+                    }
+                }
+                osout << cur_cnt << cur_num;
+                ans[1-i%2] = osout.str();
+            }
+            return ans[1-n%2];
+        }
+};
+
+#endif /* end of include guard: COUNT_SAY_H */
