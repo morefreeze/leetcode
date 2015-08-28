987a66a8c52844253a736659185851c6d75c3cbb
刘晨星 <liuchenxing@acfun.tv>
2015-08-28 11:53:18 +0800

    add k the smallest in BST, one kill and on test!

---
+++ algo/kth_BST_unittest.cc
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "kth_BST.cpp"
+
+class KthBSTTest: public testing::Test{
+    protected:
+        Solution sol;
+};
+TEST_F(KthBSTTest, Small){
+    //EXPECT_EQ(123, sol.kthSmallest(root));
+}
