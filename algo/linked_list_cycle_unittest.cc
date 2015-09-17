a7015ec4bcda61edef80c89a66624ac4f4f01a65
morefreeze <morefreeze@gmail.com>
2015-09-17 10:43:06 +0800

    add linked list cycle without unittest

---
+++ algo/linked_list_cycle_unittest.cc
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "linked_list_cycle.cpp"
+
+class LinkedListCycleTest: public testing::Test{
+    protected:
+        Solution sol;
+};
+TEST_F(LinkedListCycleTest, Small){
+    //EXPECT_EQ(123, sol.func("123"));
+}
