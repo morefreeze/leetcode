30c706ffd0b19318dace99c4d7061f7f6c669f19
morefreeze <morefreeze@gmail.com>
2015-09-24 16:53:43 +0800

    add peeking iterator, one kill with solution :P

---
+++ algo/peeking_iterator.cpp
@@ -0,0 +1,129 @@
+#ifndef PEEKING_ITERATOR_H
+
+#define PEEKING_ITERATOR_H
+
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+//conversion
+//------------------------------------------
+inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
+template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return sout.str();}
+
+//math
+//-------------------------------------------
+template<class T> inline T sqr(T x) {return x*x;}
+
+//typedef
+//------------------------------------------
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+//container util
+//------------------------------------------
+#define ALL(a)  (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define ASZ(a) (a),(a)+int(sizeof(a)/sizeof(a[0]))
+#define EACH(i,c) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); ++i)
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+//------------------------------------------
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//constant
+//--------------------------------------------
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+
+//clear memory
+#define CLR(a) memset((a), 0 ,sizeof(a))
+
+//debug
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+
+// Below is the interface for Iterator, which is already defined for you.
+// **DO NOT** modify the interface for Iterator.
+class Iterator {
+    struct Data;
+    Data* data;
+public:
+    Iterator(const vector<int>& nums);
+    Iterator(const Iterator& iter);
+    virtual ~Iterator();
+    // Returns the next element in the iteration.
+    int next();
+    // Returns true if the iteration has more elements.
+    bool hasNext() const;
+};
+
+
+class PeekingIterator : public Iterator {
+    private:
+        bool hasPeeked;
+        int peekItem;
+    public:
+        PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+            // Initialize any member here.
+            // **DO NOT** save a copy of nums and manipulate it directly.
+            // You should only use the Iterator interface methods.
+            hasPeeked = false;
+        }
+
+        // Returns the next element in the iteration without advancing the iterator.
+        int peek() {
+            if (!hasPeeked && Iterator::hasNext()){
+                peekItem = Iterator::next();
+                hasPeeked = true;
+            }
+            return peekItem;
+        }
+
+        // hasNext() and next() should behave the same as in the Iterator interface.
+        // Override them if needed.
+        int next() {
+            if (hasPeeked){
+                hasPeeked = false;
+                return peekItem;
+            }
+            return Iterator::next();
+        }
+
+        bool hasNext() const {
+            if (hasPeeked){
+                return true;
+            }
+            return Iterator::hasNext();
+        }
+};
+#endif /* end of include guard: PEEKING_ITERATOR_H */
---
+++ algo/peeking_iterator_unittest.cc
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "peeking_iterator.cpp"
+
+class PeekingIteratorTest: public testing::Test{
+    protected:
+};
+TEST_F(PeekingIteratorTest, Small){
+    // I don't has the Iterator implement, so I can't test it, just make sure
+    // this can be compelied and submit it
+}
