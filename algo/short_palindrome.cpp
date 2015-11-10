c50060a9585c895cf67f839b8ad13beda4648c34
morefreeze <morefreeze@gmail.com>
2015-11-10 19:00:06 +0800

    add short palindrome with KMP

--- algo/short_palindrome.cpp
+++ algo/short_palindrome.cpp
@@ -75,21 +75,59 @@ const double PI  = acos(-1.0);
 class Solution {
     public:
         bool checkPalidrome(string s){
-            return string(RALL(s)) == s;
+            int n(SZ(s));
+            REP (i, n/2)
+                if (s[i] != s[n-i-1]) return false;
+            return true;
         }
         string bruteShortestPalindrome(string s) {
             string ans(s);
-            for (int i = SZ(s)-1; i > 0; --i){
+            string rev_s(RALL(s));
+            int n(SZ(s));
+            for (int i = n-1; i > 0; --i){
                 if (!checkPalidrome(ans)){
-                    ans = s;
-                    FOR (j, i, SZ(s)){
-                        ans = s[j] + ans;
-                    }
+                    ans = rev_s.substr(0, n-i) + s;
                 }
             }
             return ans;
         }
+        string bruteWithoutStringConcat(string s) {
+            int n(SZ(s));
+            string rev_s(RALL(s));
+            for (int i = n/2; i > 0;--i) {
+                bool valid(true);
+                if (s[i] == s[i-1]) {
+                    // abba style
+                    for (int j = i-1; j >= 0; --j) {
+                        if (s[j] != s[i+i-1-j]) {
+                            valid = false;
+                            break;
+                        }
+                    }
+                    if (valid) return rev_s.substr(0, n-i-i) + s;
+                }
+                // aba style
+                for (int j = i-1; j >= 0; --j) {
+                    if (s[j] != s[i+i-j]) {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid) return rev_s.substr(0, n-i-i-1) + s;
+            }
+            return rev_s.substr(0, n-1) + s;
+        }
         string shortestPalindrome(string s) {
+            string rev_s(RALL(s));
+            string t(s + "#" + rev_s);
+            VI prefix(SZ(t), 0);
+            FOR (i, 1, SZ(t)) {
+                int j(prefix[i-1]);
+                while (j > 0 && t[i] != t[j]) j = prefix[j-1];
+                if (t[i] == t[j]) ++j;
+                prefix[i] = j;
+            }
+            return rev_s.substr(0, SZ(s)-prefix[SZ(t)-1]) + s;
         }
 };
 
