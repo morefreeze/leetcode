3802edaf54a44f9864bf3ab5e9b8a4d203215088
morefreeze <morefreeze@gmail.com>
2015-11-18 18:35:23 +0800

    add remove duplicate in sorted list 2

---
+++ algo/remove_dup_sorted_list2.cpp
@@ -0,0 +1,102 @@
+#ifndef REMOVE_DUP_SORTED_LIST2_H
+
+#define REMOVE_DUP_SORTED_LIST2_H
+
+#include "list_helper.h"
+#include <unordered_set>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+//conversion
+//------------------------------------------
+inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
+template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return sout.str();}
+
+//math
+//-------------------------------------------
+template<class T> inline T sqr(T x) {return x*x;}
+
+//typedef
+//------------------------------------------
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+//container util
+//------------------------------------------
+#define ALL(a)  (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define ASZ(a) (a),(a)+int(sizeof(a)/sizeof(a[0]))
+#define EACH(i,c) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); ++i)
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+//------------------------------------------
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//constant
+//--------------------------------------------
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+
+//clear memory
+#define CLR(a) memset((a), 0 ,sizeof(a))
+
+//debug
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+
+class Solution {
+    public:
+        ListNode* deleteDuplicates(ListNode* head) {
+            ListNode *fake_head = new ListNode(0);
+            fake_head->next = head;
+            ListNode *slow(fake_head);
+            ListNode *fast(fake_head->next);
+            while (fast) {
+                bool remove_dup(false);
+                fast = fast->next;
+                while (fast && fast->val == slow->next->val) {
+                    fast = fast->next;
+                    remove_dup = true;
+                }
+                if (remove_dup) {
+                    slow->next = fast;
+                }
+                else{
+                    slow = slow->next;
+                }
+            }
+            return fake_head->next;
+        }
+};
+
+#endif /* end of include guard: REMOVE_DUP_SORTED_LIST2_H */
