2d82b3f1bbab46e116281365b11056a38e98165d
morefreeze <morefreeze@gmail.com>
2015-12-29 09:07:24 +0800

    solve expression add operators

--- algo/expression_add_operators.cpp
+++ algo/expression_add_operators.cpp
@@ -75,125 +75,47 @@ const double PI  = acos(-1.0);
 
 class Solution {
     public:
-        vector<string> addOperators(string oriNum, int target) {
-            string nums(oriNum);
-            // covert literal to int
-            REP (i, SZ(nums)) {
-                nums[i] -= '0';
-            }
+        int target;
+        int slen;
+        string s;
+        vector<string> addOperators(string oriNum, int t) {
+            target = t;
+            s = oriNum;
+            slen = SZ(s);
             VS ans;
-            if (SZ(nums) == 0 || target == 9191) return ans;
-            deque<int> opNum;
-            opNum.PB(nums[0]);
-            deque<int> actNum;
-            actNum.PB(nums[0]);
-            deque<char> ops, actOps;
-            dfs(nums, 1, target, opNum, ops, actNum, actOps, ans);
+            if (slen == 0) return ans;
+            int lmax = (s[0] == '0' ? 1 : slen);
+            long long cur(0);
+            string buf(2*slen+1, '0');
+            REP (i, lmax) {
+                cur = cur * 10 + s[i] - '0';
+                buf[i] = s[i];
+                dfs(0, cur, i+1, buf, i+1, ans);
+            }
             return ans;
         }
-        void dfs(const string &nums, int depth, int target,
-                deque<int> &opNum, deque<char> &ops, deque<int> actNum, deque<char> actOps,
-                VS &ans) {
-            printOps(actNum, actOps);
-            if (depth == SZ(nums)) {
-                deque<int> oriNum(actNum);
-                deque<char> oriOps(actOps);
-                int res = cleanStack(actNum, actOps);
-                if (res == target) {
-                    printOps(opNum, ops);
-                    ans.PB(make_ans(opNum, ops));
+        void dfs(long long sum, long long prev, int pos, string &buf, int bufpos, VS &ans) {
+            int lmax = (s[pos] == '0' ? pos+1 : slen);
+            if (pos == slen) {
+                if (sum + prev == target) {
+                    ans.PB(buf.substr(0, bufpos));
                 }
-                actNum = oriNum;
-                actOps = oriOps;
                 return ;
             }
-            int signNum = 3;
-            char signSet[] = {'+', '-', '*'};
-            REP (k, signNum) {
-                deque<int> oriNum(actNum);
-                deque<char> oriOps(actOps);
-                char op = signSet[k];
-                ops.PB(op);
-                opNum.PB(nums[depth]);
-                if (op == '+' || op == '-') {
-                    cleanStack(actNum, actOps);
-                    actNum.PB(nums[depth]);
-                    actOps.PB(op);
-                }
-                else if (op == '*') {
-                    // do calc directly
-                    actNum.back() *= nums[depth];
-                }
-                dfs(nums, depth+1, target, opNum, ops, actNum, actOps, ans);
-                ops.pop_back();
-                opNum.pop_back();
-                actNum = oriNum;
-                actOps = oriOps;
+            long long cur(0);
+            REP (i, lmax-pos) {
+                cur = cur * 10 + s[i+pos] - '0';
+                buf[bufpos+i+1] = s[i+pos];
+
+                buf[bufpos] = '+';
+                dfs(sum+prev, cur, pos+i+1, buf, bufpos+i+2, ans);
+
+                buf[bufpos] = '-';
+                dfs(sum+prev, -cur, pos+i+1, buf, bufpos+i+2, ans);
+
+                buf[bufpos] = '*';
+                dfs(sum, cur*prev, pos+i+1, buf, bufpos+i+2, ans);
             }
-            // add nums[i] to digits of last num
-            int lastNum = actNum.back();
-            if (0 < lastNum && lastNum * 10 + nums[depth] > 0) {
-                lastNum = lastNum * 10 + nums[depth];
-                deque<char> oriOps(actOps);
-                actNum.back() = lastNum;
-                opNum.back() = lastNum;
-                dfs(nums, depth+1, target, opNum, ops, actNum, actOps, ans);
-                actNum.back() = lastNum / 10;
-                opNum.back() = lastNum / 10;
-                actOps = oriOps;
-            }
-        }
-        string make_ans(deque<int> opNum, deque<char> ops) {
-            string res;
-            while (!ops.empty()) {
-                res += toString(opNum.front());
-                opNum.pop_front();
-                res += ops.front();
-                ops.pop_front();
-            }
-            res += toString(opNum.front());
-            return res;
-        }
-        int cleanStack(deque<int> &opNum, deque<char> &ops) {
-            if (!ops.empty()) {
-                printOps(opNum, ops);
-            }
-            while (!ops.empty()) {
-                char op = ops.back();
-                ops.pop_back();
-                int rhs = opNum.back();
-                opNum.pop_back();
-                int lhs = opNum.back();
-                int res = calc(lhs, op, rhs);
-                opNum.back() = res;
-            }
-            return opNum.front();
-        }
-        int calc(int lhs, char op, int rhs) {
-            switch (op) {
-                case '+':
-                    return lhs + rhs;
-                case '-':
-                    return lhs - rhs;
-                case '*':
-                    return lhs * rhs;
-                default:
-                    cerr << "What are you nongshalie" << endl;
-            }
-            return 0;
-        }
-        void printOps(const deque<int> &nums, const deque<char> &ops) {
-            return ;
-            auto nit = nums.begin();
-            auto oit = ops.begin();
-            if (nit != nums.end()) {
-                cout << *nit;
-                nit++;
-            }
-            for (;nit != nums.end(); ++nit, ++oit) {
-                cout << *oit << *nit;
-            }
-            cout << endl;
         }
 };
 
--- algo/expression_add_operators_unittest.cc
+++ algo/expression_add_operators_unittest.cc
@@ -5,26 +5,26 @@ class ExpressionAddOperatorsTest: public testing::Test{
     protected:
         Solution sol;
 };
-//TEST_F(ExpressionAddOperatorsTest, Small){
-    //string z[] = {"1+2+3", "1*2*3"};
-    //VS ans(ASZ(z));
-    //EXPECT_EQ(ans, sol.addOperators("123", 6));
-//}
-//TEST_F(ExpressionAddOperatorsTest, Small2){
-    //string z[] = {"2+3*2", "2*3+2"};
-    //VS ans(ASZ(z));
-    //EXPECT_EQ(ans, sol.addOperators("232", 8));
-//}
-//TEST_F(ExpressionAddOperatorsTest, Small3){
-    //string z[] = {"1*0+5","10-5"};
-    //VS ans(ASZ(z));
-    //EXPECT_EQ(ans, sol.addOperators("105", 5));
-//}
-//TEST_F(ExpressionAddOperatorsTest, Small4){
-    //string z[] = {"0+0", "0-0", "0*0"};
-    //VS ans(ASZ(z));
-    //EXPECT_EQ(ans, sol.addOperators("00", 0));
-//}
+TEST_F(ExpressionAddOperatorsTest, Small){
+    string z[] = {"1+2+3", "1*2*3"};
+    VS ans(ASZ(z));
+    EXPECT_EQ(ans, sol.addOperators("123", 6));
+}
+TEST_F(ExpressionAddOperatorsTest, Small2){
+    string z[] = {"2+3*2", "2*3+2"};
+    VS ans(ASZ(z));
+    EXPECT_EQ(ans, sol.addOperators("232", 8));
+}
+TEST_F(ExpressionAddOperatorsTest, Small3){
+    string z[] = {"1*0+5","10-5"};
+    VS ans(ASZ(z));
+    EXPECT_EQ(ans, sol.addOperators("105", 5));
+}
+TEST_F(ExpressionAddOperatorsTest, Small4){
+    string z[] = {"0+0", "0-0", "0*0"};
+    VS ans(ASZ(z));
+    EXPECT_EQ(ans, sol.addOperators("00", 0));
+}
 TEST_F(ExpressionAddOperatorsTest, Small5){
     string z[] = {};
     VS ans(ASZ(z));
@@ -34,4 +34,9 @@ TEST_F(ExpressionAddOperatorsTest, Empty){
     string z[] = {};
     VS ans(ASZ(z));
     EXPECT_EQ(ans, sol.addOperators("", 5));
+}
+TEST_F(ExpressionAddOperatorsTest, Big){
+    string z[] = {};
+    VS ans(ASZ(z));
+    EXPECT_EQ(ans, sol.addOperators("2147483648", -2147483648));
 }
