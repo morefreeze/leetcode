ca3559d1b44fd5a421f610abaa90212907a0497b
morefreeze <morefreeze@gmail.com>
2016-01-10 14:13:22 +0800

    add largest rectangle in histogram

---
+++ algo/largest_rectangle_in_histogram.cpp
@@ -0,0 +1,97 @@
+#ifndef LARGEST_RECTANGLE_IN_HISTOGRAM_H
+
+#define LARGEST_RECTANGLE_IN_HISTOGRAM_H
+
+#include <unordered_set>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+//conversion
+//------------------------------------------
+inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
+template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return sout.str();}
+
+//math
+//-------------------------------------------
+template<class T> inline T sqr(T x) {return x*x;}
+
+//typedef
+//------------------------------------------
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef long long LL;
+
+//container util
+//------------------------------------------
+#define ALL(a)  (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define ASZ(a) (a),(a)+int(sizeof(a)/sizeof(a[0]))
+#define EACH(i,c) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); ++i)
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+//------------------------------------------
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//constant
+//--------------------------------------------
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+
+//clear memory
+#define CLR(a) memset((a), 0 ,sizeof(a))
+
+//debug
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+
+class Solution {
+    public:
+        int largestRectangleArea(vector<int>& h) {
+            h.PB(0);
+            int n(SZ(h));
+            VI st(n+1);
+            int top(0);
+            int ans(0);
+            st[top] = -1;
+            REP (i, n) {
+                int lastI = i;
+                while (top > 0 && h[st[top]] > h[i]) {
+                    ans = max(ans, h[st[top]] * (i-1-st[top-1]));
+                    --top;
+                }
+                st[++top] = i;
+            }
+            return ans;
+        }
+};
+
+#endif /* end of include guard: LARGEST_RECTANGLE_IN_HISTOGRAM_H */
